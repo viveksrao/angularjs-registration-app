748ef8865bd196f5ebc21e14aa45f067509b7070
Vivekanand Rao <vivek.rao@teachscape.com>
2015-11-11 13:15:29 -0800

    Route Setup.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-var myApp = angular.module('myRegistrationApp',[]);
+var myApp = angular.module('myRegistrationApp',['ng-route']);
 
-myApp.controller('registrationAppController',['$scope',function($scope){
-	$scope.message = 'Welcome to AngularJS Registration App';
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'RegistrationController'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html',
+			controller: 'RegistrationController'
+		}).
+		when('/success', {
+			templateUrl: 'views/success.html',
+			controller: 'SuccessController'
+		}).
+		otherwise({
+			redirectTo: '/login'
+		});
 }]);
