494f23d2d832147ba52337449fe1dfd79a03d12a
Vivekanand Rao <vivek.rao@teachscape.com>
2015-11-11 11:18:23 -0800

    Created a basic MVC structure.

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('myRegistrationApp',[]);
+
+myApp.controller('registrationAppController',['$scope',function($scope){
+	$scope.message = 'Welcome to AngularJS Registration App';
+}]);
