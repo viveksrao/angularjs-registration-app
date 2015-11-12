5db384f0d09c3cbe7be532923b02cdf9e88a9dd9
Vivekanand Rao <vivek.rao@teachscape.com>
2015-11-12 15:28:27 -0800

    Added Control on authentication on pages.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ var myApp = angular.module('myRegistrationApp',
 						   ['ngRoute','firebase'])
 							.constant('FIREBASE_URL','https://angjsregistration.firebaseio.com/');
 
+myApp.run(['$rootScope','$location', function($rootScope, $location){
+	$rootScope.$on('$routeChangeError', function(event, next, previous, error){
+		if(error='AUTH_REQUIRED'){
+			$rootScope.message = 'Sorry, you must log in to access that page';
+			$location.path('/login');
+		}
+	});
+}]);
+
+
 myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 		when('/login', {
@@ -14,7 +24,12 @@ myApp.config(['$routeProvider', function($routeProvider){
 		}).
 		when('/success', {
 			templateUrl: 'views/success.html',
-			controller: 'SuccessController'
+			controller: 'SuccessController',
+			resolve: {
+				currentAuth: function(Authentication){
+					return Authentication.requireAuth();
+				}
+			}
 		}).
 		otherwise({
 			redirectTo: '/login'
--- js/services/authentication.js
+++ js/services/authentication.js
@@ -28,6 +28,10 @@ myApp.factory('Authentication',['$rootScope','$firebaseAuth','$firebaseObject','
 			return auth.$unauth();
 		}, // logout
 		
+		requireAuth: function(){
+			return auth.$requireAuth();
+		},// requireAuth
+		
 		register: function(user){
 			auth.$createUser({
 				email: user.email,
